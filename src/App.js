3ee1de07d24a7795df4560a4a6ef8af688478402
Zach Howes <zhowes14@icloud.com>
2022-03-04 13:42:15 -0500

    Set up react router and created each page file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Resume from "./Components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Resume" element={<Resume />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Resume.js
@@ -0,0 +1,9 @@
+const Resume = () => {
+  return (
+    <div>
+      <h1>Resumé</h1>
+    </div>
+  );
+};
+
+export default Resume;
