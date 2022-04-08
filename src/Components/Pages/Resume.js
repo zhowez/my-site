b6aca73b2f8573c879d109fca7dcafa8cdd3732f
Zach Howes <zhowes14@icloud.com>
2022-04-08 11:49:18 -0400

    Added University info

--- src/Components/Pages/Resume.js
+++ src/Components/Pages/Resume.js
@@ -6,7 +6,23 @@ const Resume = () => {
     <div>
       <Navbar />
       <div className="Content-Resume">
-        <div className="Left-Side">Left Side</div>
+        <div className="Left-Side">
+          <div className="Education">
+            <h1>Education</h1>
+            <hr className="Line-Education" />
+            <div className="Education-Uni">
+              <h2>Clemson University</h2>
+              <h2>2018-2022</h2>
+            </div>
+            <div>
+              <h2>Bachelor of Science: Computer Science</h2>
+              <div className="Education-Awards">
+                <h3>GPA: 3.52</h3>
+                <h3>Dean's List: Fall 2019, Fall 2020, Fall 2021</h3>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="Right-Side">Right Side</div>
       </div>
     </div>
