f38248dde3e7c77ac975e0f87990262dc2056781
Zach Howes <zhowes14@icloud.com>
2022-07-23 20:28:14 -0400

    added post for contact form

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,11 +1,51 @@
 import Footer from "../Footer/Footer";
 import style from "./Contact.module.css";
 import ContentBox from "../UI/ContentBox";
+import { useState } from "react";
 
 const Contact = () => {
-  const submitHandler = (event) => {
+  const submitHandler = async (event) => {
     event.preventDefault();
+
+    const date = new Date();
+    const info = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+      date: date.toISOString().split("T")[0],
+      time: date.toLocaleTimeString(),
+    };
+    console.log(info);
+    const res = await fetch(
+      "https://personal-site-a9a2c-default-rtdb.firebaseio.com/responses.json",
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(info),
+      }
+    );
+
+    if (!res.ok) {
+      alert("something went wrong");
+    }
+    setName("");
+    setEmail("");
+    setVerifyEmail("");
+    setSubject("");
+    setMessage("");
+    alert("Thank you for filling out the contact form.");
   };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [verifyEmail, setVerifyEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <div className={style.content}>
@@ -16,25 +56,50 @@ const Contact = () => {
             <div className={style["organize-form"]}>
               <span className={style["organize-inputs"]}>
                 <label className={style.name}>Name </label>
-                <input></input>
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                ></input>
               </span>
               <div className={style["organize-email"]}>
                 <span className={style["organize-inputs"]}>
                   <label className={style.email}>Email </label>
-                  <input></input>
+                  <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  ></input>
                 </span>
                 <span className={style["organize-inputs"]}>
                   <label className={style.email}>Re-Enter Email </label>
-                  <input></input>
+                  <input
+                    type="email"
+                    name="verifyEmail"
+                    value={verifyEmail}
+                    onChange={(event) => setVerifyEmail(event.target.value)}
+                  ></input>
                 </span>
               </div>
               <span className={style["organize-inputs"]}>
                 <label className={style.subject}>Subject </label>
-                <input></input>
+                <input
+                  type="text"
+                  name="subject"
+                  value={subject}
+                  onChange={(event) => setSubject(event.target.value)}
+                ></input>
               </span>
               <span className={style["organize-inputs"]}>
                 <label>Message </label>
-                <textarea></textarea>
+                <textarea
+                  type="text"
+                  name="message"
+                  value={message}
+                  onChange={(event) => setMessage(event.target.value)}
+                ></textarea>
               </span>
 
               <button>Submit</button>
