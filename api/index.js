dd8dced0b89a55a30e5b291dcb8e774778d41060
Zach Howes <zhowes14@icloud.com>
2022-07-26 23:17:09 -0400

    added forward to database

--- api/index.js
+++ api/index.js
@@ -1,14 +1,29 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+import fetch from "node-fetch";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api", (req, res) => {
+app.post("/api", async (req, res) => {
   const body = req.body;
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(body.email);
+
+  const answer = await fetch(
+    "https://personal-site-a9a2c-default-rtdb.firebaseio.com/responses.json",
+    {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    }
+  );
+
+  if (!answer.ok) {
+    return res.status(404);
+  }
+  return res.status(200);
 });
 
 module.exports = app;
