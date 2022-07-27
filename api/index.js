7bde8797620974b7c8aa18aa7d61e8f6c1cae848
Zach Howes <zhowes14@icloud.com>
2022-07-26 22:51:03 -0400

    seeing if name will send back

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ app.post("/api", (req, res) => {
   const body = req.body;
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.json(body);
+  res.json(body.name);
 });
 
 module.exports = app;
